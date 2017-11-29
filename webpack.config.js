ec0a73c71c2d3296639e9a09212eba6bfdb24219
danielatherton1 <daniel.atherton@skybettingandgaming.com>
2017-11-29 15:29:02 +0000

    Webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@
 // https://nodejs.org/api/path.html
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 // Constant with our paths
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
   JS: path.resolve(__dirname, 'src/js'),
 };
 
@@ -15,4 +18,15 @@ module.exports = {
     path: paths.DIST,
     filename: 'app.bundle.js'
   },
+// Tell webpack to use html plugin -> ADDED IN THIS STEP
+// index.html is used as a template in which it'll inject bundled app.
+plugins: [
+  new HtmlWebpackPlugin({
+    template: path.join(paths.SRC, 'index.html'),
+  }),
+],
+
+// devServer: {
+//     contentBase: paths.SRC,
+//   }
 };
