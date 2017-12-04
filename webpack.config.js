2d0e2e16a3d7bfb8406ccef729612dac8058d146
danielatherton1 <daniel.atherton@skybettingandgaming.com>
2017-12-04 16:50:16 +0000

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@
 // https://nodejs.org/api/path.html
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 // Constant with our paths
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
   JS: path.resolve(__dirname, 'src/js'),
 };
 
@@ -13,6 +16,35 @@ module.exports = {
   entry: path.join(paths.JS, 'app.js'),
   output: {
     path: paths.DIST,
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+  },
+  // Tell webpack to use html plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+  // Loaders configuration -> ADDED IN THIS STEP
+  // We are telling webpack to use "babel-loader" for .js and .jsx files
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
+  //
+  // So we can write:
+  // import MyComponent from './my-component';
+  //
+  // Instead of:
+  // import MyComponent from './my-component.jsx';
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
